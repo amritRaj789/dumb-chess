45b4a358e247884aaa4e4eab54e48226b2025e53
Amrit Raj <amritrazzz@gmail.com>
2021-02-09 23:10:05 +0530

    made squares of alternating background color

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,9 @@
 const Square = (props) => {
-
+	const sumOfIndices = props.index[0] + props.index[1];
 	const style = {
 		width: "100px",
 		height: "100px",
-		background: "#141414",
+		background: ((sumOfIndices%2 === 0) ? "#141414" : "#AAA"),
 		color: "white"
 	}
 
